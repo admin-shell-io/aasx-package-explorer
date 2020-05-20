d74da43efb5e9c274ba6c97900d41687fb1f4297
Marko Ristin <marko.ristin@gmail.com>
2020-05-20 20:25:16 +0000

    Add PowerShell scripts for building and development (#19)
    This commit adds powershell scripts to automate installation of
    development and solution dependencies as well as building and checking
    the solution.
    
    This is particularly handy when you need to develop or test the building
    pipeline on a virtual machine.

---
+++ src/FormatCode.ps1
@@ -0,0 +1,17 @@
+﻿if(!(Test-Path env:DOTNET_ROOT)) {
+    $dotnetRoot = Join-Path $env:LOCALAPPDATA "Microsoft\dotnet"
+    if(!(Test-Path $dotnetRoot)) {
+        throw "Expected dotnet root to exist, but it does not: $dotnetRoot"
+    }
+
+    Write-Host "Setting DOTNET_ROOT environment variable to: $dotnetRoot"
+    [Environment]::SetEnvironmentVariable(
+        "DOTNET_ROOT",
+        $dotnetRoot)
+}
+
+$dotnetFormatPath=Join-Path $env:UserProfile ".dotnet\tools\dotnet-format.exe"
+
+cd $PSScriptRoot
+
+& $dotnetFormatPath
