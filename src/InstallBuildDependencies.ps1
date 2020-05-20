891369bb1c4df6aebcb2ee250025360a8c1dc127
Marko Ristin <marko.ristin@gmail.com>
2020-05-20 20:25:16 +0000

    Add PowerShell scripts for building and development (#19)
    This commit adds powershell scripts to automate installation of
    development and solution dependencies as well as building and checking
    the solution.
    
    This is particularly handy when you need to develop or test the building
    pipeline on a virtual machine.

---
+++ src/InstallBuildDependencies.ps1
@@ -0,0 +1,16 @@
+﻿# This script installs the dependencies necessary to build the solution.
+# The script uses nuget and the dependencies are stored in the packages/ 
+# subdirectory.
+
+if ((Get-Command "nuget.exe" -ErrorAction SilentlyContinue) -eq $null) 
+{ 
+   throw "Unable to find nuget.exe in your PATH"
+}
+
+cd $PSScriptRoot
+
+Write-Host "Installing .NET compilers 2.10.0 ..."
+nuget install Microsoft.NET.Compilers -Version 2.10.0
+
+Write-Host "Restoring packages for the solution ..."
+nuget.exe restore AasxPackageExplorer.sln
