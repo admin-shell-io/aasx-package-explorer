fdb52aea41f65b6e1e20c609dacca56703fbc28a
Marko Ristin <marko.ristin@gmail.com>
2020-05-25 16:42:01 +0000

    Add nuget install nunit console runner

---
+++ src/CheckFormat.ps1
@@ -0,0 +1,24 @@
+﻿<#
+This script checks the format of the code.
+#>
+
+$ErrorActionPreference = "Stop"
+
+Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
+    AssertDotnet, `
+    AssertDotnetFormatVersion
+
+
+AssertDotnet
+AssertDotnetFormatVersion
+
+cd $PSScriptRoot
+Write-Host "Inspecting the code format with dotnet-format..."
+
+$reportPath = Join-Path $PSScriptRoot "dotnet-format-report.json"
+dotnet format --dry-run --report $reportPath
+$formatReport = Get-Content $reportPath |ConvertFrom-Json
+if($formatReport.Count -ge 1) {
+    throw "There are $($formatReport.Count) dotnet-format issue(s). " + `
+        "The report is stored in: $reportPath"
+}
---
+++ src/InspectCode.ps1
@@ -0,0 +1,29 @@
+﻿<#
+This script inspects the code quality.
+
+It should be run after the build.
+#>
+
+$ErrorActionPreference = "Stop"
+
+Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
+    FindInspectCode
+
+$inspectcode = FindInspectCode
+
+Write-Host "Inspecting the code with inspectcode.exe ..."
+
+# InspectCode passes over the properties to MSBuild,
+# see https://www.jetbrains.com/help/resharper/InspectCode.html#msbuild-related-parameters
+& $inspectcode `
+    --properties:Configuration=Debug `
+    --properties:Platform=x64 `
+    "-o=resharper-code-inspection.xml" `
+    AasxPackageExplorer.sln
+ 
+[xml]$inspection = Get-Content "resharper-code-inspection.xml"
+$issueCount = $inspection.SelectNodes('//Issue').Count
+if( $issueCount -ne 0 ) {
+    throw "There are $issueCount InspectCode issue(s). " + `
+        "The issues are stored in: $PSScriptRoot\resharper-code-inspection.xml"
+}
