b4f35586bcb4a3ac2afeed006bead64f48072845
Marko Ristin <marko.ristin@gmail.com>
2020-05-27 07:02:38 +0000

    Add code coverage
    This adds OpenCover and ReportGenerator to the CI pipeline so that we
    get code coverage from our unit tests.
    
    Additionally, this change required us to refactor `src/Check.ps1` since
    we could not maintain its complexity anymore. Now with this change,
    `src/Check.ps1` simply calls the other scripts.
    
    Finally, the the artefacts and tools overcrowded `src/` so now all
    building and reporting is stored to `artefacts/` directory and all build
    dependencies are installed in `tools/` directory. Both directories are
    gitignored on purpose.

--- src/InspectCode.ps1
+++ src/InspectCode.ps1
@@ -7,9 +7,13 @@ It should be run after the build.
 $ErrorActionPreference = "Stop"
 
 Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
-    FindInspectCode
+    FindInspectCode, `
+    CreateAndGetArtefactsDir
 
 $inspectcode = FindInspectCode
+
+$artefactsDir = CreateAndGetArtefactsDir
+$codeInspectionPath = Join-Path $artefactsDir "resharper-code-inspection.xml"
 
 Write-Host "Inspecting the code with inspectcode.exe ..."
 
@@ -18,12 +22,13 @@ Write-Host "Inspecting the code with inspectcode.exe ..."
 & $inspectcode `
     --properties:Configuration=Debug `
     --properties:Platform=x64 `
-    "-o=resharper-code-inspection.xml" `
+    "-o=$codeInspectionPath" `
     AasxPackageExplorer.sln
  
-[xml]$inspection = Get-Content "resharper-code-inspection.xml"
+
+[xml]$inspection = Get-Content $codeInspectionPath
 $issueCount = $inspection.SelectNodes('//Issue').Count
 if( $issueCount -ne 0 ) {
     throw "There are $issueCount InspectCode issue(s). " + `
-        "The issues are stored in: $PSScriptRoot\resharper-code-inspection.xml"
+        "The issues are stored in: $codeInspectionPath"
 }
--- src/InstallBuildDependencies.ps1
+++ src/InstallBuildDependencies.ps1
@@ -7,14 +7,26 @@ if ((Get-Command "nuget.exe" -ErrorAction SilentlyContinue) -eq $null)
    throw "Unable to find nuget.exe in your PATH"
 }
 
-cd $PSScriptRoot
+$toolsDir = Join-Path (Split-Path $PSScriptRoot -Parent) "tools"
+New-Item -ItemType Directory -Force -Path $toolsDir
+
+Write-Host "Installing the tools in: $toolsDir"
 
 Write-Host "Installing .NET compilers 2.10.0 ..."
-nuget install Microsoft.NET.Compilers -Version 2.10.0
+nuget install Microsoft.NET.Compilers -Version 2.10.0 `
+    -OutputDirectory $toolsDir
 
 Write-Host "Installing Nunit Console Runner ..."
-nuget install NUnit.ConsoleRunner -Version 3.11.1
-nuget install NUnit.Extension.NUnitProjectLoader -Version 3.6.0
+nuget install NUnit.ConsoleRunner -Version 3.11.1 `
+    -OutputDirectory $toolsDir
+
+nuget install NUnit.Extension.NUnitProjectLoader -Version 3.6.0 `
+    -OutputDirectory $toolsDir
+
+Write-Host "Installing OpenCover ..."
+nuget install OpenCover -Version 4.7.922 -OutputDirectory $toolsDir
+nuget install ReportGenerator -Version 4.6.0 -OutputDirectory $toolsDir
 
 Write-Host "Restoring packages for the solution ..."
+cd $PSScriptRoot
 nuget.exe restore AasxPackageExplorer.sln
