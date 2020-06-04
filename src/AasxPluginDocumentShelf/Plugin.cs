df76ce7d6f99c7bb152e6d78ba737689b330595a
Marko Ristin <marko.ristin@gmail.com>
2020-06-04 18:57:40 +0200

    Remove Eclipse license from the code (#44)
    The Eclipse Foundation license is a legacy from when the code based
    lived on Eclipse Foundation servers and was mandated by it. We changed
    the license to Apache 2 during the migration from Gitlab to Github
    repository.

--- src/AasxPluginDocumentShelf/Plugin.cs
+++ src/AasxPluginDocumentShelf/Plugin.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 
 /* Copyright (c) 2018-2019 Festo AG & Co. KG <https://www.festo.com/net/de_de/Forms/web/contact_international>, author: Michael Hoffmeister
-This software is licensed under the Eclipse Public License 2.0 (EPL-2.0) (see https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt).
 The Newtonsoft.JSON serialization is licensed under the MIT License (MIT).
 The Microsoft Microsoft Automatic Graph Layout, MSAGL, is licensed under the MIT license (MIT).
 */
