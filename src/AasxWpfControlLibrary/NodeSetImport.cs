0523af7c9412c13ad7048b2298ce54cce24f6ad1
Andreas Orzelski <aorzelski@phoenixcontact.com>
2020-06-09 07:56:45 +0200

    Import extended

--- src/AasxWpfControlLibrary/NodeSetImport.cs
+++ src/AasxWpfControlLibrary/NodeSetImport.cs
@@ -11,6 +11,12 @@ using AdminShellNS;
 namespace AasxPackageExplorer
 {
 
+    public class field
+    {
+        public string name;
+        public string value;
+        public string description;
+    }
 
     public class UaNode
     {
@@ -29,10 +35,15 @@ namespace AasxPackageExplorer
         public List<UaNode> children;
         public List<string> references;
 
+        public string DefinitionName;
+        public string DefinitionNameSpace;
+        public List<field> fields;
+
         public UaNode()
         {
             children = new List<UaNode> { };
             references = new List<string> { };
+            fields = new List<field> { };
         }
     }
 
@@ -40,9 +51,7 @@ namespace AasxPackageExplorer
     {
         static List<UaNode> roots;
         static List<UaNode> nodes;
-        static Dictionary<string,UaNode> parentNodes;
-        static UaNode[] stack;
-        static int iStack = 0;
+        static Dictionary<string, UaNode> parentNodes;
 
         public static void ImportNodeSetToSubModel(string inputFn, AdminShell.AdministrationShellEnv env, AdminShell.Submodel sm, AdminShell.SubmodelRef smref)
         {
@@ -50,13 +59,14 @@ namespace AasxPackageExplorer
             StreamWriter sw = File.CreateText(inputFn + ".log.txt");
 
             string elementName = "";
+            string lastElementName = "";
+            bool tagDefinition = false;
             string referenceType = "";
 
             roots = new List<UaNode> { };
             nodes = new List<UaNode> { };
             parentNodes = new Dictionary<string, UaNode>();
             UaNode currentNode = null;
-            iStack = 0;
 
             // global model data
             string ModelUri = "";
@@ -114,6 +124,25 @@ namespace AasxPackageExplorer
                             case "Reference":
                                 referenceType = reader.GetAttribute("ReferenceType");
                                 break;
+                            case "Definition":
+                                tagDefinition = true;
+                                currentNode.DefinitionName = reader.GetAttribute("Name");
+                                var splitd = currentNode.DefinitionName.Split(':');
+                                if (splitd.Length > 1)
+                                {
+                                    currentNode.DefinitionNameSpace = splitd[0];
+                                    if (splitd.Length == 2)
+                                        currentNode.DefinitionName = splitd[1];
+                                }
+                                break;
+                            case "Field":
+                                field f = new field();
+                                f.name = reader.GetAttribute("Name");
+                                f.value = reader.GetAttribute("Value");
+                                currentNode.fields.Add(f);
+                                break;
+                            case "Description":
+                                break;
                         }
                         break;
                     case XmlNodeType.Text:
@@ -127,7 +156,18 @@ namespace AasxPackageExplorer
                                 currentNode.Value = reader.Value;
                                 break;
                             case "Description":
-                                currentNode.Description = reader.Value;
+                                if (tagDefinition)
+                                {
+                                    int count = currentNode.fields.Count;
+                                    if (count > 0)
+                                    {
+                                        currentNode.fields[count - 1].description = reader.Value;
+                                    }
+                                }
+                                else
+                                {
+                                    currentNode.Description = reader.Value;
+                                }
                                 break;
                             case "Reference":
                                 string reference = referenceType + " " + reader.Value;
@@ -139,6 +179,12 @@ namespace AasxPackageExplorer
                         }
                         break;
                     case XmlNodeType.EndElement: //Display the end of the element.
+                        switch (reader.Name)
+                        {
+                            case "Definition":
+                                tagDefinition = false;
+                                break;
+                        }
                         if (currentNode == null || currentNode.UAObjectTypeName == null)
                         {
                             break;
@@ -168,6 +214,7 @@ namespace AasxPackageExplorer
                                     break;
                             }
                         }
+                        lastElementName = reader.Name;
                         break;
                 }
             }
@@ -338,161 +385,14 @@ namespace AasxPackageExplorer
             {
                 p.semanticId.Keys.Add(AdminShell.Key.CreateNew("UAReference", false, "OPC", s));
             }
-        }
-
-        public static void ImportNodeSetToSubModel_OLD(string inputFn, AdminShell.AdministrationShellEnv env, AdminShell.Submodel sm, AdminShell.SubmodelRef smref)
-        {
-            // OPC UA NodeSet
-            // Input
-            Boolean is_Iri = false;
-            String Uri = "";
-            Boolean is_UADataType = false;
-            Boolean is_UAVariable = false;
-            Boolean is_UAObject = false;
-            Boolean is_UAMethod = false;
-            Boolean is_DisplayName = false;
-            Boolean is_Value = false;
-            String DisplayName = "";
-            // String Field_Name = "";
-            // String Field_Value = "";
-            // Output
-            String Name = "";
-            String Value = "";
-
-            AdminShell.SubmodelElementCollection[] propGroup = new AdminShell.SubmodelElementCollection[10];
-            int i_propGroup = 0;
-
-            XmlTextReader reader = new XmlTextReader(inputFn);
-            StreamWriter sw = File.CreateText(inputFn + ".log.txt");
-
-            while (reader.Read())
+            if (n.DefinitionName != null && n.DefinitionName != "")
+                p.semanticId.Keys.Add(AdminShell.Key.CreateNew("UADefinitionName", false, "OPC", n.DefinitionName));
+            if (n.DefinitionNameSpace != null && n.DefinitionNameSpace != "")
+                p.semanticId.Keys.Add(AdminShell.Key.CreateNew("UADefinitionNameSpace", false, "OPC", n.DefinitionNameSpace));
+            foreach (field f in n.fields)
             {
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element: // The node is an element.
-                        if (reader.Name == AdminShell.Identification.IRI)
-                            is_Iri = true;
-                        if (reader.Name == "UADataType")
-                            is_UADataType = true;
-                        if (reader.Name == "UAVariable")
-                            is_UAVariable = true;
-                        if (reader.Name == "UAObject")
-                            is_UAObject = true;
-                        if (reader.Name == "UAMethod")
-                            is_UAMethod = true;
-                        if (reader.Name == "DisplayName")
-                            is_DisplayName = true;
-                        if (reader.Name == "Value")
-                            is_Value = true;
-                        if (reader.Name == "Field")
-                        {
-                            Name = reader.GetAttribute("Name");
-                            Value = reader.GetAttribute("Value");
-                        }
-                        break;
-                    case XmlNodeType.Text: //Display the text in each element.
-                        if (is_Iri)
-                        {
-                            Uri = reader.Value;
-                        }
-                        if (is_DisplayName)
-                        {
-                            DisplayName = reader.Value;
-                        }
-                        if (is_UAVariable && is_Value)
-                        {
-                            if (reader.Value.Length < 100)
-                            {
-                                Value = reader.Value;
-                            }
-                            else
-                            {
-                                Value = reader.Value.Substring(0, 100) + " ..";
-                            }
-                        }
-                        break;
-                    case XmlNodeType.EndElement: //Display the end of the element.
-                        if (is_Iri)
-                        {
-                            Name = "NamespaceURI";
-                            Value = Uri;
-                            is_Iri = false;
-                            Uri = "";
-                        }
-                        if (is_UADataType && is_DisplayName)
-                        {
-                            propGroup[i_propGroup] = AdminShell.SubmodelElementCollection.CreateNew("UADataType " + DisplayName);
-                            sm.Add(propGroup[i_propGroup]);
-                            i_propGroup++;
-                            is_UADataType = false;
-                            is_DisplayName = false;
-                            DisplayName = "";
-                        }
-                        if (reader.Name == "UADataType")
-                            i_propGroup--;
-                        if (reader.Name == "UAVariable")
-                        {
-                            Name = "UAVariable " + DisplayName;
-                            DisplayName = "";
-                            is_DisplayName = false;
-                            is_UAVariable = false;
-                        }
-                        if (is_UAObject)
-                        {
-                            Name = "UAObject " + DisplayName;
-                            DisplayName = "";
-                            is_DisplayName = false;
-                            is_UAObject = false;
-                        }
-                        if (is_UAMethod)
-                        {
-                            Name = "UAMethod " + DisplayName;
-                            DisplayName = "";
-                            is_DisplayName = false;
-                            is_UAMethod = false;
-                        }
-                        if (is_Value)
-                        {
-                            is_Value = false;
-                        }
-                        if (is_DisplayName)
-                        {
-                            is_DisplayName = false;
-                        }
-                        break;
-                }
-                if (Name != "")
-                {
-                    using (var cd = AdminShell.ConceptDescription.CreateNew(AdminShell.Identification.IRDI, ""))
-                    {
-                        env.ConceptDescriptions.Add(cd);
-                        cd.SetIEC61360Spec(
-                            preferredNames: new[] { "EN", Name },
-                            shortName: Name,
-                            unit: "string",
-                            valueFormat: "STRING",
-                            definition: new[] { "EN", Name }
-                        );
-
-                        var p = AdminShell.Property.CreateNew(cd.GetDefaultShortName(), "PARAMETER", AdminShell.Key.GetFromRef(cd.GetReference()));
-                        p.valueType = "string";
-                        p.value = Value;
-
-                        if (i_propGroup > 0)
-                        {
-                            propGroup[i_propGroup - 1].Add(p);
-                        }
-                        else
-                        {
-                            sm.Add(p);
-                        }
-                    }
-                    Name = "";
-                    Value = "";
-                }
+                p.semanticId.Keys.Add(AdminShell.Key.CreateNew("UAField", false, "OPC", f.name + " = " + f.value + " : " + f.description));
             }
-            sw.Close();
         }
-
     }
 }
