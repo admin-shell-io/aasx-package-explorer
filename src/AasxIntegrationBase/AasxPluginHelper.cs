959e42fc919ac9e03c28cfa5d844dda2aad1ad5b
MichaelHoffmeisterFesto <miho@de.festo.com>
2020-08-06 11:53:48 +0200

    Fix one re-sharper comment
    * fix

--- src/AasxIntegrationBase/AasxPluginHelper.cs
+++ src/AasxIntegrationBase/AasxPluginHelper.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+// ReSharper disable ClassNeverInstantiated.Global
+
 namespace AasxIntegrationBase
 {
     public class AasxPluginHelper
