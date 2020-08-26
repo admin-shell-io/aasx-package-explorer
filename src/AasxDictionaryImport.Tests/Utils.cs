c751ff204eed50bd7065c5568ad995d8c2cc93fe
Marko Ristin <marko.ristin@gmail.com>
2020-08-26 18:33:00 +0200

    Add unit tests for IEC CDD import
    This patch adds the AasxImport.Tests project that contains unit tests
    for the IEC CDD import implemented in the AasxImport project.
    
    (This commit was originally written by krahlro-sick. It was impossible
    to rebase the original commit since the LICENSE.txt's changed too much
    in the meanwhile. mristin only re-comitted the changes and fixed the
    conflicts.)

---
+++ src/AasxDictionaryImport.Tests/Utils.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright (c) 2020 SICK AG <info@sick.de>
+ *
+ * This software is licensed under the Apache License 2.0 (Apache-2.0).
+ * The ExcelDataReder dependency is licensed under the MIT license
+ * (https://github.com/ExcelDataReader/ExcelDataReader/blob/develop/LICENSE).
+ */
+
+#nullable enable
+
+using System;
+using System.IO;
+
+namespace AasxDictionaryImport.Tests
+{
+    /// <summary>
+    /// A wrapper around a path string that will recursively delete the directory at the wrapped path when it is
+    /// disposed (if the directory exists).
+    /// </summary>
+    internal sealed class TempDir : IDisposable
+    {
+        public string Path { get; }
+
+        private TempDir(string path)
+        {
+            Path = path;
+            Directory.CreateDirectory(Path);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(Path))
+                Directory.Delete(Path, true);
+        }
+
+        /// <summary>
+        /// Generate and create a new temporary directory.
+        /// </summary>
+        /// <returns>A new temporary directory that has been created on the file system</returns>
+        public static TempDir Create() => Create(System.IO.Path.GetTempPath());
+
+        /// <summary>
+        /// Generate and create a new temporary directory within the given base directory.
+        /// </summary>
+        /// <param name="baseDir">The base directory for the created directory</param>
+        /// <returns>A new temporary directory that has been created on the file system</returns>
+        public static TempDir Create(string baseDir)
+        {
+            var tempDir = Generate(baseDir);
+            Directory.CreateDirectory(tempDir);
+            return tempDir;
+        }
+
+        /// <summary>
+        /// Generate a temporary directory but do not create it.
+        /// </summary>
+        /// <returns>A new temporary directory that has not yet been created on the file system</returns>
+        public static TempDir Generate() => Generate(System.IO.Path.GetTempPath());
+
+        /// <summary>
+        /// Generate a temporary directory within the given base directory but do not create it.
+        /// </summary>
+        /// <param name="baseDir">The base directory for the generated directory</param>
+        /// <returns>A new temporary directory that has not yet been created on the file system</returns>
+        public static TempDir Generate(string baseDir)
+        {
+            return new TempDir(System.IO.Path.Combine(baseDir, System.IO.Path.GetRandomFileName()));
+        }
+
+        public static implicit operator string(TempDir tempDir) => tempDir.Path;
+    }
+
+    internal sealed class WorkingDirectory : IDisposable
+    {
+        public string Path;
+
+        private WorkingDirectory(string path)
+        {
+            Path = path;
+        }
+
+        public void Dispose()
+        {
+            Directory.SetCurrentDirectory(Path);
+        }
+
+        public static WorkingDirectory ChangeTo(string path)
+        {
+            var oldPath = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(path);
+            return new WorkingDirectory(oldPath);
+        }
+    }
+}
