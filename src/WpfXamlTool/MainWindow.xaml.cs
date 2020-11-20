3839de3a663340af79f08c6f04f6619804380438
Marko Ristin <marko.ristin@gmail.com>
2020-11-20 16:01:40 +0100

    Make `*.cs` and `*.xaml` headers uniform
    This patch makes all the headers in the source code uniform to include
    the expected copyright notice(s), the license of the software as well
    as references to other open source projects used by the software.
    
    Where there was no copyright notice previously and it was obvious that
    the code was written for AASX Package Explorer we put Michael
    Hoffmeister at Festo 2018-2019 as the copyright holder.
    
    The copyright notices from third-party source codes such as Identity
    Server 4 are left as-are.

--- src/WpfXamlTool/MainWindow.xaml.cs
+++ src/WpfXamlTool/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+Copyright (c) 2018-2019 Festo AG & Co. KG <https://www.festo.com/net/de_de/Forms/web/contact_international>
+Author: Michael Hoffmeister
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
