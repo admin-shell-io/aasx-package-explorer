b1fcfd05b0469d0c2341e6b23a08a8160fe42463
Andreas Orzelski <aorzelski@phoenixcontact.com>
2020-12-22 15:33:14 +0100

    Add push and pop to walk through tree without recursion
    AnyUiPanels are iterated through their children to display flat HTML

--- src/BlazorUI/Program.cs
+++ src/BlazorUI/Program.cs
@@ -15,6 +15,41 @@ namespace BlazorUI
 {
     public class Program
     {
+        public class AnyUiPanelEntry
+        {
+            public AnyUiPanel panel;
+            public int iChild;
+            public AnyUiPanelEntry() { }
+           
+        }
+
+        public class AnyUiPanelEntryStack
+        {
+            AnyUiPanelEntry[] recursionStack = new AnyUiPanelEntry[10];
+            public int iRecursionStack = 0;
+            public AnyUiPanelEntryStack() { }
+            public int getIndex() { return iRecursionStack; }
+            public void Pop(out AnyUiPanel panel, out int iChild)
+            {
+                panel = null;
+                iChild = 0;
+                if (iRecursionStack > 0)
+                {
+                    iRecursionStack--;
+                    panel = recursionStack[iRecursionStack].panel;
+                    iChild = recursionStack[iRecursionStack].iChild;
+                    recursionStack[iRecursionStack] = null;
+                }
+            }
+            public void Push(AnyUiPanel panel, int iChild)
+            {
+                recursionStack[iRecursionStack] = new Program.AnyUiPanelEntry();
+                recursionStack[iRecursionStack].panel = panel;
+                recursionStack[iRecursionStack].iChild = iChild + 1;
+                iRecursionStack++;
+            }
+        }
+
         public static AdminShellPackageEnv env = null;
 
         public static AnyUiStackPanel stack = new AnyUiStackPanel();
@@ -45,21 +80,35 @@ namespace BlazorUI
 
             if (true)
             {
-                var lab = new AnyUiLabel();
-                lab.Content = "Hallo";
-                lab.Foreground = AnyUiBrushes.DarkBlue;
-                stack.Children.Add(lab);
+                var lab1 = new AnyUiLabel();
+                lab1.Content = "Hallo1";
+                lab1.Foreground = AnyUiBrushes.DarkBlue;
+                stack.Children.Add(lab1);
 
-                //var stck2 = new AnyUiStackPanel();
-                //stck2.Orientation = AnyUiOrientation.Horizontal;
-                //stack.Children.Add(stck2);
+                var stck2 = new AnyUiStackPanel();
+                stck2.Orientation = AnyUiOrientation.Horizontal;
+                stack.Children.Add(stck2);
+
+                var lab2 = new AnyUiLabel();
+                lab2.Content = "Hallo2";
+                lab2.Foreground = AnyUiBrushes.DarkBlue;
+                stck2.Children.Add(lab2);
+
+                var stck3 = new AnyUiStackPanel();
+                stck3.Orientation = AnyUiOrientation.Horizontal;
+                stck2.Children.Add(stck3);
+
+                var lab3 = new AnyUiLabel();
+                lab3.Content = "Hallo3";
+                lab3.Foreground = AnyUiBrushes.DarkBlue;
+                stck3.Children.Add(lab3);
 
                 if (editMode)
                 {
                     var tb = new AnyUiTextBox();
                     tb.Foreground = AnyUiBrushes.Black;
                     tb.Text = "Initial";
-                    stack.Children.Add(tb);
+                    stck2.Children.Add(tb);
                     //repo.RegisterControl(tb, (o) =>
                     //{
                     //    Log.Singleton.Info($"Text changed to .. {"" + o}");
@@ -73,7 +122,7 @@ namespace BlazorUI
                         if (o == btn)
                             Program.LogLine = "Hallo, Match zwischen Button und callback!";
                     });
-                    stack.Children.Add(btn);
+                    stck3.Children.Add(btn);
                     //repo.RegisterControl(btn, (o) =>
                     //{
                     //    Log.Singleton.Error("Button clicked!");
