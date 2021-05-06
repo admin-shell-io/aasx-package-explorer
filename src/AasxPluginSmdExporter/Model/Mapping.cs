1b8a1517cfaf75dd7e6c7cbd30d73eb730d7a176
Sebastian Bischof <sebastian.bischof@keb.de>
2021-05-06 08:45:53 +0200

    Add SMD exporter plugin to the project
    The SMD exporter plugin creates a simulation-tool independent
    Simulation Model Description.
    This description includes all important information
    for creating a simulation model.
    
    Based on the BillOfMaterial submodel.
    Based on information from the SimulationModels submodel.
    License information changed

---
+++ src/AasxPluginSmdExporter/Model/Mapping.cs
@@ -0,0 +1,38 @@
+﻿/*
+Copyright (c) 2021 KEB Automation KG <https://www.keb.de/>,
+Copyright (c) 2021 Lenze SE <https://www.lenze.com/en-de/>,
+author: Jonas Grote, Denis Göllner, Sebastian Bischof
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AasxPluginSmdExporter
+{
+    public class Mapping
+    {
+        public string IdShort { get; set; }
+
+        public string UnknownId { get; set; }
+
+        public string BasicId { get; set; }
+
+        public double Value { get; set; }
+
+        public Mapping(string idShort, string unknownId, string basicId, double value)
+        {
+            this.IdShort = idShort;
+            this.UnknownId = unknownId;
+            this.BasicId = basicId;
+            this.Value = value;
+        }
+
+    }
+}
