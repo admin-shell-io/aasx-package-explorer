42e809301d076da1200bf5e9e0a8c270cd756adb
Michael Hoffmeister <michael.hoffmeister@festo.com>
2022-04-06 14:53:37 +0200

    Update sources
    * start import of time series

---
+++ src/AasxPluginExportTable/ImportTimeSeriesFlyout.xaml.cs
@@ -0,0 +1,140 @@
+﻿/*
+Copyright (c) 2018-2021 Festo AG & Co. KG <https://www.festo.com/net/de_de/Forms/web/contact_international>
+Author: Michael Hoffmeister
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AasxIntegrationBase;
+using AasxPluginExportTable.TimeSeries;
+using AasxPluginExportTable.Uml;
+using AdminShellNS;
+using Newtonsoft.Json;
+
+namespace AasxPluginExportTable
+{
+    public partial class ImportTimeSeriesFlyout : UserControl, IFlyoutControl
+    {
+        public event IFlyoutControlAction ControlClosed;
+
+        protected string _caption;
+
+        public ImportTimeSeriesOptions Result = new ImportTimeSeriesOptions();
+
+        //
+        // Init
+        //
+
+        public ImportTimeSeriesFlyout(string caption = null)
+        {
+            InitializeComponent();
+            _caption = caption;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // combo Formats
+            ComboBoxFormat.Items.Clear();
+            foreach (var f in ImportTimeSeriesOptions.FormatNames)
+                ComboBoxFormat.Items.Add("" + f);
+
+            // set given values
+            ThisFromPreset(Result);
+        }
+
+        //
+        // Outer
+        //
+
+        public void ControlStart()
+        {
+        }
+
+        public void ControlPreviewKeyDown(KeyEventArgs e)
+        {
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Result = null;
+            ControlClosed?.Invoke();
+        }
+
+        //
+        // Mechanics
+        //
+
+        private ImportTimeSeriesOptions ThisToPreset()
+        {
+            var x = new ImportTimeSeriesOptions();
+
+            x.Format = (ImportTimeSeriesOptions.FormatEnum)ComboBoxFormat.SelectedIndex;
+
+            int i = 0;
+
+            if (int.TryParse(TextBoxRowHeader.Text, out i))
+                x.RowData = i;
+
+            if (int.TryParse(TextBoxRowData.Text, out i))
+                x.RowData = i;
+
+            if (int.TryParse(TextBoxColumnTime.Text, out i))
+                x.ColTime = i;
+
+            if (int.TryParse(TextBoxColumnData.Text, out i))
+                x.ColData = i;
+
+            if (int.TryParse(TextBoxNumData.Text, out i))
+                x.NumData = i;
+
+            x.SetSmSemantic = CheckBoxSetSmSemId.IsChecked == true;
+
+            return x;
+        }
+
+        private void ThisFromPreset(ImportTimeSeriesOptions preset)
+        {
+            // access
+            if (preset == null)
+                return;
+
+            // take over
+            ComboBoxFormat.SelectedIndex = (int)preset.Format;
+            TextBoxRowHeader.Text = "" + preset.RowHeader;
+            TextBoxRowData.Text = "" + preset.RowData;
+            TextBoxColumnTime.Text = "" + preset.ColTime;
+            TextBoxColumnData.Text = "" + preset.ColData;
+            TextBoxNumData.Text = "" + preset.NumData;
+            CheckBoxSetSmSemId.IsChecked = preset.SetSmSemantic;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == ButtonStart)
+            {
+                this.Result = this.ThisToPreset();
+                ControlClosed?.Invoke();
+            }
+        }
+    }
+}
---
+++ src/AasxPluginExportTable/TimeSeries/ImportTimeSeries.cs
@@ -0,0 +1,101 @@
+﻿/*
+Copyright (c) 2018-2022 Festo AG & Co. KG <https://www.festo.com/net/de_de/Forms/web/contact_international>
+Author: Michael Hoffmeister
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Xml;
+using System.Xml.Schema;
+using ClosedXML.Excel;
+using AasxIntegrationBase;
+using AasxIntegrationBase.AasForms;
+using AdminShellNS;
+using Newtonsoft.Json;
+
+namespace AasxPluginExportTable.TimeSeries
+{
+    /// <summary>
+    /// This class allows importing a Submodel for TimeSeries from various formats.
+    /// Note: it is a little misplaced in the "export table" plugin, however the
+    /// domain is quite the same and maybe special file format dependencies will 
+    /// be re equired in the future.
+    /// </summary>
+    public class ImportTimeSeries
+    {
+        //
+        // Public interface
+        //
+        
+        public static void ImportTimeSeriesFromFile(
+            AdminShell.AdministrationShellEnv env,
+            AdminShell.Submodel submodel,
+            ImportTimeSeriesOptions options,
+            string fn)
+        {
+            // access
+            if (options == null | !fn.HasContent())
+                return;
+
+            // which writer?
+            if (options.Format == ImportTimeSeriesOptions.FormatEnum.Excel)
+                ;
+
+        }
+
+        //
+        // Internal
+        //
+
+        protected class TimeSeriesRow
+        {
+            public DateTime? TimeStamp = null;
+            public List<double> Data = new List<double>();
+        }
+
+        protected List<TimeSeriesRow> _rows = null;
+
+        protected int _maxRows = 0, _maxCols = 0;
+
+        protected List<TimeSeriesRow> ImportExcel(
+            ImportTimeSeriesOptions options,
+            string fn)
+        {
+            // access
+            if (!fn.HasContent())
+                return null;
+
+            // open excel
+            var wb = new XLWorkbook(fn);
+            if (wb?.Worksheets == null || wb.Worksheets.Count < 1)
+                return null;
+
+            // only 1st worksheet
+            var _worksheet = (wb.Worksheets).ToList()[0];
+
+            // check rows, cols
+            _maxRows = (_worksheet?
+                .LastRowUsed(XLCellsUsedOptions.Contents) == null)
+                ? 0 : _worksheet.LastRowUsed(XLCellsUsedOptions.Contents).RowNumber();
+
+            _maxCols = (_worksheet?
+                .LastColumnUsed(XLCellsUsedOptions.Contents) == null)
+                ? 0 : _worksheet.LastColumnUsed(XLCellsUsedOptions.Contents).ColumnNumber();
+
+            if (_maxRows < 1 || _maxCols < 1)
+                return null;
+        }
+    }
+}
