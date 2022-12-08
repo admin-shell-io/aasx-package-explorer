b25c262c9ad5bbca670dff4efff3f1a5d60aae71
Matthias Freund <matthias.freund@festo.com>
2022-12-08 09:55:10 +0100

    Enhancements and fixes for evaluation of XML fragments

--- src/AasxRestServerLibrary/AasxHttpContextHelperXmlExtensions.cs
+++ src/AasxRestServerLibrary/AasxHttpContextHelperXmlExtensions.cs
@@ -25,16 +25,8 @@ namespace AasxRestServerLibrary
         {
             try
             {
-                XmlDocument xmlDocument = LoadXmlDocument(xmlFileStream);
-                XPathNodeIterator fragmentObjectsIterator = FindFragmentObjects(xmlDocument, xmlFragment);
-
-                if (fragmentObjectsIterator.Count > 1)
-                {
-                    throw new XmlFragmentEvaluationException($"Fragment evaluation did return multiple XML elements. Only xPath expressions returning a single element are supported.");
-                }
-
-                fragmentObjectsIterator.MoveNext();
-                XPathNavigator fragmentObject = fragmentObjectsIterator.Current;
+                XDocument xmlDocument = LoadXmlDocument(xmlFileStream);
+                IEnumerable<XObject> fragmentObjects = FindFragmentObjects(xmlDocument, xmlFragment);
 
                 var content = context.Request.QueryString.Get("content") ?? "normal";
                 var level = context.Request.QueryString.Get("level") ?? "deep";
@@ -42,25 +34,38 @@ namespace AasxRestServerLibrary
 
                 if (level == "core")
                 {
-                    DeeplyNestedXmlElementsRemover.RemoveDeeplements(fragmentObject);
+                    foreach (var fragmentObject in fragmentObjects)
+                    {
+                        if (fragmentObject.NodeType == XmlNodeType.Element)
+                        {
+                            RemoveDeeplements(fragmentObject as XElement);
+                        }
+                    }
                 }
 
-                if (content == "xml") {
+                if (content == "xml")
+                {
+                    if (fragmentObjects.Count() > 1)
+                    {
+                        throw new XmlFragmentEvaluationException($"Fragment evaluation did return multiple XML elements. Only xPath expressions returning a single element are supported when returning xml content.");
+                    }
 
-                    if (fragmentObject.NodeType != XPathNodeType.Element)
+                    var fragmentObject = fragmentObjects.First();
+
+                    if (fragmentObject.NodeType != XmlNodeType.Element)
                     {
                         throw new XmlFragmentEvaluationException($"Fragment evaluation did not return an Element but a(n) " + fragmentObject.NodeType + "!");
                     }
-                    XElement xmlElement = XElement.Parse(fragmentObject.OuterXml);
 
-                    SendXmlResponse(context, xmlElement);
+                    SendXmlResponse(context, fragmentObject as XElement);
 
-                } else
+                }
+                else
                 {
 
                     JsonConverter converter = new XmlJsonConverter(xmlFragment, content, extent);
-                    string json = JsonConvert.SerializeObject(fragmentObject, Newtonsoft.Json.Formatting.Indented, converter);
-                
+                    string json = JsonConvert.SerializeObject(fragmentObjects, Newtonsoft.Json.Formatting.Indented, converter);
+
                     SendJsonResponse(context, json);
                 }
 
@@ -75,13 +80,11 @@ namespace AasxRestServerLibrary
             }
         }
 
-        private static XmlDocument LoadXmlDocument(Stream xmlFileStream)
+        public static XDocument LoadXmlDocument(Stream xmlFileStream)
         {
             try
             {
-                var doc = new XmlDocument();
-                doc.Load(xmlFileStream);
-                return doc;
+                return XDocument.Load(xmlFileStream);
             }
             catch
             {
@@ -89,23 +92,44 @@ namespace AasxRestServerLibrary
             }
         }
 
-        private static XPathNodeIterator FindFragmentObjects(XmlDocument xmlDocument, string xmlFragment)
+        private static IEnumerable<XObject> FindFragmentObjects(XDocument xmlDocument, string xmlFragment)
         {
-            var xPath = xmlFragment.Trim('/');
+            // select the root element if the fragment is empty
+            var xPath = (xmlFragment == null || xmlFragment.Length == 0) ? "/*" : xmlFragment;
 
-            XPathNavigator navigator = xmlDocument.CreateNavigator();
-            XPathExpression query;
+            XmlNamespaceManager manager = CreateNamespaceManager(xmlDocument);
 
+            object result;
             try
             {
-                query = navigator.Compile(xPath);
+                result = xmlDocument.XPathEvaluate(xPath, manager);
             }
             catch
             {
                 throw new XmlFragmentEvaluationException($"Unable to compile xPath query '" + xPath + "'.");
             }
 
-            // register the namespace prefixes used in the XML document so that they can be used in xPath queries
+            IEnumerable<XObject> nodes;
+            try
+            {
+                nodes = ((IEnumerable<object>)result).Cast<XObject>();
+            }
+            catch
+            {
+                throw new XmlFragmentEvaluationException($"Evaluating xPath query '" + xPath + "' did not return a node list.");
+            }
+
+            if (nodes.Count() == 0)
+            {
+                throw new XmlFragmentEvaluationException($"Evaluating xPath query '" + xPath + "' did not return a result.");
+            }
+
+            return nodes;
+        }
+
+        private static XmlNamespaceManager CreateNamespaceManager(XDocument xmlDocument)
+        {
+            XPathNavigator navigator = xmlDocument.CreateNavigator();
             XmlNamespaceManager manager = new XmlNamespaceManager(navigator.NameTable);
             navigator.MoveToFollowing(XPathNodeType.Element);
             IDictionary<string, string> namespaces = navigator.GetNamespacesInScope(XmlNamespaceScope.All);
@@ -113,17 +137,27 @@ namespace AasxRestServerLibrary
             {
                 manager.AddNamespace(ns.Key, ns.Value);
             }
-            navigator.MoveToRoot();
 
-            query.SetContext(manager);
-            XPathNodeIterator nodes = navigator.Select(query);
+            return manager;
+        }
 
-            if (nodes.Count == 0)
+        /**
+         * A utility method that can be used to remove 'deeply nested elements' from an XML element, i.e. elements that
+         * are descendants but no direct children of the given object(s).
+         */
+        public static void RemoveDeeplements(XNode fragmentObject)
+        {
+
+            if (fragmentObject.NodeType == XmlNodeType.Element)
             {
-                throw new XmlFragmentEvaluationException($"Evaluating xPath query '" + xPath + "' did not return a result.");
-            }
+                // select all children's children (the elements to be deleted)
+                XElement nodeToDelete;
 
-            return nodes;
+                while ((nodeToDelete = fragmentObject.XPathSelectElement("./*/*")) != null)
+                {
+                    nodeToDelete.Remove();
+                }
+            }
         }
 
         static void SendJsonResponse(IHttpContext context, string json)
@@ -147,17 +181,6 @@ namespace AasxRestServerLibrary
             context.Response.ContentLength64 = txt.Length;
             context.Response.SendResponse(txt);
         }
-
-        static void SendTextResponse(IHttpContext context, object element, string mimeType = "text/plain")
-        {
-            string txt = element.ToString();
-            context.Response.ContentType = ContentType.TXT;
-            if (mimeType != null)
-                context.Response.Advanced.ContentType = mimeType;
-            context.Response.ContentEncoding = Encoding.UTF8;
-            context.Response.ContentLength64 = txt.Length;
-            context.Response.SendResponse(txt);
-        }
     }
 
     /**
@@ -165,23 +188,23 @@ namespace AasxRestServerLibrary
      * 'extent' as defined by "Details of the AAS, part 2".
      * 
      * Note: The serialization algorithm for 'content=normal' is based on directly converting the XML node to JSON.
-     */ 
+     */
     class XmlJsonConverter : JsonConverter
     {
         string BaseXpath;
         string Content;
         string Extent;
-        
+
         public XmlJsonConverter(string xmlFragment, string content = "normal", string extent = "withoutBlobValue")
         {
-            this.BaseXpath = HttpUtility.UrlDecode(xmlFragment.Trim('/')); ; 
+            this.BaseXpath = xmlFragment;
             this.Content = content;
             this.Extent = extent;
         }
 
         public override bool CanConvert(Type objectType)
         {
-            return typeof(XPathNavigator).IsAssignableFrom(objectType);
+            return typeof(IEnumerable<XObject>).IsAssignableFrom(objectType);
         }
         public override bool CanRead
         {
@@ -195,45 +218,120 @@ namespace AasxRestServerLibrary
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            XPathNavigator navigator = value as XPathNavigator;
-
-            if (navigator == null)
+            IEnumerable<XObject> nodeList;
+            try
             {
-                throw new XmlFragmentEvaluationException("Unable to convert object to XPathNavigator: " + value);
+                nodeList = (IEnumerable<XObject>)value;
+            }
+            catch
+            {
+                throw new XmlFragmentEvaluationException("Unable to convert object to IEnumerable<XObject>: " + value);
             }
 
-            JContainer result;
+            JContainer result = ConvertToJson(nodeList);
+            result.WriteTo(writer);
 
+            return;
+
+        }
+
+        private JContainer ConvertToJson(IEnumerable<XObject> nodeList)
+        {
+            if (nodeList.Count() == 1)
+            {
+                return ConvertToJson(nodeList.First());
+            }
+            else
+            {
+                JArray result = new JArray();
+
+                foreach (var node in nodeList)
+                {
+                    result.Add(ConvertToJson(node));
+                }
+
+                return result;
+            }
+        }
+
+        private JContainer ConvertToJson(XObject node)
+        {
+            JContainer result;
 
             if (Content == "value")
             {
-                result = new JObject();
-                result["value"] = navigator.InnerXml;
-            } else
+                // in case of a value-only serialization, we remove all comments and namespace-related information,
+                // i.e. namespace declarations, namespace prefixes as well as schema location information
+                var nodeWithoutNamespaces = RemoveAllNamespacesAndComments(node);
+                result = JObject.FromObject(nodeWithoutNamespaces);
+            }
+            else if (Content == "normal")
             {
-                if (navigator.NodeType != XPathNodeType.Element)
+                result = JObject.FromObject(node);
+            }
+            else if (Content == "path")
+            {
+                if (node.NodeType != XmlNodeType.Element)
                 {
-                    throw new XmlFragmentEvaluationException($"Unable to convert XML fragment to XElement. Xpath evaluation probably did return a(n) " + navigator.NodeType + " instead!");
+                    throw new XmlFragmentEvaluationException($"Fragment evaluation did not return an Element but a(n) " + node.NodeType + ". This is not supported when returning path information!");
                 }
 
-                XElement xmlElement = XElement.Parse(navigator.OuterXml);
+                var elementXpath = (BaseXpath == null || BaseXpath.Length == 0 || BaseXpath == "/*") ? "/" + GetLocalXpathExpression(node as XElement) : BaseXpath;
 
-                if (Content == "normal")
-                {
-                    result = JObject.FromObject(xmlElement);
-                } else if (Content == "path") { 
-
-                    List<string> paths = CollectChildXpathPathsRecursively(xmlElement, BaseXpath);
-                    result = JArray.FromObject(paths);
-                } else
-                {
-                    throw new XmlFragmentEvaluationException("Unsupported content modifier: " + Content);
-                }
+                List<string> paths = CollectChildXpathPathsRecursively(node as XElement, elementXpath);
+                result = JArray.FromObject(paths);
+            }
+            else
+            {
+                throw new XmlFragmentEvaluationException("Unsupported content modifier: " + Content);
             }
 
-            result.WriteTo(writer);
-            return;
+            return result;
+        }
 
+        private static XObject RemoveAllNamespacesAndComments(XObject xmlObject)
+        {
+            if (xmlObject is XElement)
+            {
+                XElement original = xmlObject as XElement;
+                XElement copy = new XElement(original.Name.LocalName);
+                copy.Add(original.Attributes().Where(att => !att.IsNamespaceDeclaration && att.Name.LocalName != "schemaLocation").Select(att => RemoveAllNamespacesAndComments(att)));
+                copy.Add(original.Nodes().Where(n => n.NodeType != XmlNodeType.Comment).Select(el => RemoveAllNamespacesAndComments(el)));
+
+                return copy;
+            }
+            else if (xmlObject is XAttribute)
+            {
+                XAttribute original = xmlObject as XAttribute;
+                return new XAttribute(original.Name.LocalName, original.Value);
+            }
+            else
+            {
+                return xmlObject;
+            }
+        }
+
+        private string GetLocalXpathExpression(XElement node)
+        {
+            var nodeName = node.Name;
+            if (nodeName.NamespaceName?.Length == 0)
+            {
+                // the node is not associated with a namespace, so we can simply use the local name as xPath expression
+                return nodeName.LocalName;
+            }
+
+            var ns = nodeName.Namespace;
+            var nsPrefix = node.GetPrefixOfNamespace(ns);
+
+            if (nsPrefix?.Length > 0)
+            {
+                // there is a prefix for the namespace of the node so we can use this for the xPath expression
+                return nsPrefix + ":" + nodeName.LocalName;
+            }
+
+            // the node is in the default namespace (without any prefix); hence, we need to use some special xPath syntax
+            // to be able to adress the node (see https://stackoverflow.com/a/2530023)
+            return "*[namespace-uri()='" + ns.NamespaceName + "' and local-name()='" + nodeName.LocalName + "']";
         }
 
         private List<string> CollectChildXpathPathsRecursively(XElement xmlElement, string baseXpath)
@@ -241,7 +339,7 @@ namespace AasxRestServerLibrary
             var paths = new List<string>();
             paths.Add(baseXpath);
 
-            Dictionary<XName, List<XElement>> childDict = GetChildrenSortedByName(xmlElement);
+            Dictionary<string, List<XElement>> childDict = GetChildrenSortedByName(xmlElement);
 
             foreach (var key in childDict?.Keys)
             {
@@ -260,23 +358,30 @@ namespace AasxRestServerLibrary
                 }
             }
 
+            foreach (var attribute in xmlElement.Attributes())
+            {
+                var attributeXpath = baseXpath + "/@" + attribute.Name;
+                paths.Add(attributeXpath);
+            }
+
             return paths;
         }
 
-        private static Dictionary<XName, List<XElement>> GetChildrenSortedByName(XElement xmlElement)
+        private Dictionary<string, List<XElement>> GetChildrenSortedByName(XElement xmlElement)
         {
-            var childDict = new Dictionary<XName, List<XElement>>();
+            var childDict = new Dictionary<string, List<XElement>>();
 
             foreach (var child in xmlElement?.Elements().ToList())
             {
+                var childName = GetLocalXpathExpression(child);
                 List<XElement> childrenWithSameName;
-                if (!childDict.TryGetValue(child.Name, out childrenWithSameName))
+                if (!childDict.TryGetValue(childName, out childrenWithSameName))
                 {
                     childrenWithSameName = new List<XElement>();
                 }
 
                 childrenWithSameName.Add(child);
-                childDict[child.Name] = childrenWithSameName;
+                childDict[childName] = childrenWithSameName;
             }
 
             return childDict;
@@ -284,40 +389,10 @@ namespace AasxRestServerLibrary
     }
 
     /**
-     * A utility class that can be used to remove 'deeply nested elements' from an XML element, i.e. elements that
-     * are descendants but no direct children of the given object(s).
-     */
-    class DeeplyNestedXmlElementsRemover
-    {
-        private DeeplyNestedXmlElementsRemover() { }
-
-        public static void RemoveDeeplements(XPathNavigator fragmentObject)
-        {
-            
-            if (fragmentObject.NodeType == XPathNodeType.Element)
-            {
-                XPathNodeIterator nodesToDelete;
-
-                // select all children's children (the elements to be deleted)
-                while ((nodesToDelete = fragmentObject.Select("./*/*")).Count > 0)
-                {
-
-                    nodesToDelete.MoveNext();
-                    if (nodesToDelete.Current.NodeType == XPathNodeType.Element)
-                    {
-                        nodesToDelete.Current.DeleteSelf();
-                    }
-                }
-            }
-        }
-    }
-
-
-
-    /**
      * An exception that indicates that something went wrong while evaluating an XML fragment.
      */
-    public class XmlFragmentEvaluationException : ArgumentException {
+    public class XmlFragmentEvaluationException : ArgumentException
+    {
 
         public XmlFragmentEvaluationException(string message) : base(message)
         {
