3284b9a49feb28474dd66216e0bc305ef1988131
Evgenij Boxberger <9017969+eboxberger@users.noreply.github.com>
2022-12-13 11:59:02 +0100

    Add json schema export (#589)
    The user can export the json schema based on submodel templates.
    
    In a nutshell:
    An option was added to the file menu
    "Export Submodel JSON Schema",
    The logic is implemented in a separate library
    (AasxSchemaExport)

---
+++ src/AasxSchemaExport/SubmodelElementDefinitionContext.cs
@@ -0,0 +1,44 @@
+﻿/*
+Copyright (c) 2022 PHOENIX CONTACT GmbH & Co. KG <info@phoenixcontact.de>
+
+This source code is licensed under the Apache License 2.0 (see LICENSE.txt).
+
+This source code may use other Open Source software components (see LICENSE.txt).
+*/
+
+using AdminShellNS;
+using Newtonsoft.Json.Linq;
+
+namespace AasxSchemaExport
+{
+    internal class SubmodelElementDefinitionContext
+    {
+        private const string DynamicIdShortIndicator = "{00}";
+
+        public JObject Schema { get; }
+        public JArray TargetAllOf { get; }
+        public AdminShellV20.SubmodelElement SubmodelElement { get; }
+
+        public string SubmodelElementName { get; }
+
+        public bool IsDynamicIdShort => SubmodelElement.idShort.EndsWith(DynamicIdShortIndicator);
+
+        public JObject ContainsReference { get; }
+        public JObject SubmodelElementDefinition { get; }
+
+        public JObject SubmodelElementDefinitionProperties => SubmodelElementDefinition[Tokens.Properties] as JObject;
+
+        public JObject SchemaDefinitions => Schema[Tokens.Definitions] as JObject;
+
+        public SubmodelElementDefinitionContext(JObject schema, JArray targetAllOf, AdminShellV20.SubmodelElement submodelElement)
+        {
+            Schema = schema;
+            TargetAllOf = targetAllOf;
+            SubmodelElement = submodelElement;
+
+            SubmodelElementName = SubmodelElement.idShort.Replace(DynamicIdShortIndicator, "");
+            ContainsReference = JObject.Parse($@"{{'{Tokens.Contains}': {{'{Tokens.Ref}': '#/{Tokens.Definitions}/{SubmodelElementName}'}}}}");
+            SubmodelElementDefinition = JObject.Parse($@"{{'{Tokens.Properties}': {{}}}}");
+        }
+    }
+}
