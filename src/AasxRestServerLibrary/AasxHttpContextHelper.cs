5ab7f10eb5fa327a311972cb823258fb09d18faa
Michael Hoffmeister <michael.hoffmeister@festo.com>
2023-09-07 10:01:09 +0200

    Resolving the conflict with solution file   branch

--- src/AasxRestServerLibrary/AasxHttpContextHelper.cs
+++ src/AasxRestServerLibrary/AasxHttpContextHelper.cs
@@ -18,6 +18,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AasxCompatibilityModels;
 using AdminShellNS;
 using Grapevine.Interfaces.Server;
 using Grapevine.Server;
--- src/AasxRestServerLibrary/AasxHttpHandleStore.cs
+++ src/AasxRestServerLibrary/AasxHttpHandleStore.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AasxCompatibilityModels;
 using AdminShellNS;
 using Newtonsoft.Json;
 
